9e5dffbf074bdc48ba5f87d30e55f97f9cc448c3
ramagammer <skwgrama@gmail.com>
2025-09-01 23:53:06 -0300

    Vercel speed

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./providers"; // 👈 Importá Providers
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,6 +31,7 @@ export default function RootLayout({
       >
         <Providers> {/* 👈 Ahora sí envolvemos con SessionProvider */}
           {children}
+          <SpeedInsights /> {/* 👈 acá se agrega */}
         </Providers>
       </body>
     </html>
